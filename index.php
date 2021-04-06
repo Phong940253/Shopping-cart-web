969f5a9db9ef7e31f0bdde19cbae71340852be26
Phong <phong940253@gmail.com>
2021-04-06 15:46:00 +0700

    connect login form with navbar

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/style.css">
-    <script src="./vendor/jquery.min.js"/>
+    <script src="./vendor/jquery.min.js"></script>
 </head>
 
 <body>
@@ -19,11 +19,59 @@
     ?>
     <script>
         $(document).ready(function(){
-            $("p").click(function(){
-                $(this).hide();
+            $(".header-account-container").click(function(){
+                $(".overlay").css("visibility", "visible");
             });
+            $(".btn-close").click(function () {
+                $(".overlay").css("visibility", "collapse");
+            });
+            // $(document).on("click", function(event){
+            //     if(!$(event.target).closest(".overlay-content").length){
+            //         $(".overlay").css("visibility", "collapse");
+            //     }
+            // });
         });
     </script>
+    <div class="overlay">
+        <div class="overlay-content">
+            <div class="overlay-root">
+                <button class="btn-close">
+                    <img src="../img/close-button.png"/>
+                </button>
+                <div class="style-left">
+                    <div class="style-login-with-email">
+                        <div class="heading">
+                            <h4>Đăng nhập bằng email</h4>
+                            <p>Nhập email và mật khẩu tài khoản</p>
+                        </div>
+                        <form>
+                            <div class="input input-fill">
+                                <input type="email" name="email" placeholder="acb@email.com" value="">
+                            </div>
+                            <div class="input input-fill">
+                                <input type="password" placeholder="Mật khẩu" value="">
+                                <span class="show-pass">Hiện</span>
+                            </div>
+                            <button>Đăng nhập</button>
+                        </form>
+                        <p class="forgot-pass">Quên mật khẩu?</p>
+                        <p class="create-account">
+                            Chưa có tài khoản?
+                            <span>Tạo tài khoản</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="style-right">
+                    <img src="../img/icon.png" width="203">
+                    <div class="content">
+                        <h4>Mua sắm tại Team-IT</h4>
+                        <span>Siêu ưu đãi mỗi ngày</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
