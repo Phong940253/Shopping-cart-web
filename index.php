23071f2ec3a8f77268bf5c499c76f7ce5ae308f3
Phong <phong940253@gmail.com>
2021-04-06 11:36:40 +0700

    add form login

--- index.php
+++ index.php
@@ -7,15 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/style.css">
+    <script src="./vendor/jquery.min.js"/>
 </head>
 
 <body>
     <?php
     include_once './vendor/autoload.php';
     require("./inc/header.php");
-    $connect = connect_db::getConnection();
-    connect_db::closeConnection($connect);
+//    $connect = connect_db::getConnection();
+//    connect_db::closeConnection($connect);
     ?>
+    <script>
+        $(document).ready(function(){
+            $("p").click(function(){
+                $(this).hide();
+            });
+        });
+    </script>
 </body>
 
 </html>
---
+++ modules/login.php
@@ -0,0 +1,17 @@
+<?php
+require_once "../vendor/autoload.php";
+echo '
+    <link rel="stylesheet" href="../css/style.css">
+    <div class="overlay">
+        <div class="overlay-content">
+            <div class="overlay-root">
+                <button class="btn-close">
+                    <img src="../img/close-button.png"/>
+                </button>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+'
+?>
