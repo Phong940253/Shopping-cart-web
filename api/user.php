d20352e53cc4517d0460413bc9daeed0a98d08e5
Phong <phong940253@gmail.com>
2021-05-12 02:51:54 +0700

    add post user (not hash) and get all user api

--- api/user.php
+++ api/user.php
@@ -29,7 +29,32 @@ class user extends restful_api
 
     function create() {
         if ($this->method == "POST") {
-            $this->response(200, $this->params);
+            $database = new DatabaseConnector();
+            $query = "INSERT INTO user (firstName, middleName, lastName, mobile, email, passwordHash, admin, vendor, registeredAt, intro, profile) VALUES ('" . $this->params['firstName'] . "', '" . $this->params['middleName'] . "', '" . $this->params['lastName'] . "', '" . $this->params['mobile'] . "', '" . $this->params['email'] . "', '" . $this->params['passwordHash'] . "', '" . $this->params['admin'] . "', '" . $this->params['vendor'] . "', '"  . date("Y-m-d") . "', '" . $this->params['intro'] . "', '" . $this->params['profile'] . "')";
+            $result = $database->getConnection()->query($query);
+            if ($result) {
+                $this->response(201, $this->params);
+            } else {
+                $this->response(500, "ERROR: Could not able to execute $query.");
+            }
+        }
+    }
+
+    function getall() {
+        if ($this->method == "GET") {
+            $database = new DatabaseConnector();
+            $query = "select * from user";
+            $result = $database->getConnection()->query($query);
+            $data = array();
+            try {
+                while ($row = $result->fetch_assoc()) {
+                    $data[] = $row;
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                $this->response(500, "ERROR");
+            }
+            $this->response(200, $data);
         }
     }
 }
