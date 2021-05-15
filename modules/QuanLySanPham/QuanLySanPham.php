4eeb30336ac279fd7ab7a5b6621dc0118fd148e5
TRỊNH THỊ PHƯƠNG VI <4501104276@student.hcmue.edu.vn>
2021-05-15 12:33:14 +0700

    Them muc Thong-bao-cua-toi va sua css

--- modules/QuanLySanPham/QuanLySanPham.php
+++ modules/QuanLySanPham/QuanLySanPham.php
@@ -21,14 +21,21 @@
                 </div>
                 <ul class="ThongTinTuyChon">
                     <li>
-                        <a class="is-active" href="/Nguoidung/Taikhoan/Chinhsua">
+                        <a class="Thong-tin-tai-khoan">
                             <svg stroke="currentColor" fill="currentColor" stroke-width="0"
                                  viewBox="0 0 24 24" height="1em" width="1em"
                                  xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79
-                                4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
                             </svg>
                             <span>Thông tin sản phẩm</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="Thong-bao-cua-toi">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                                 viewBox="0 0 24 24" height="1em" width="1em"
+                                 xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <span>Thông báo của  tôi</span>
                         </a>
                     </li>
                 </ul>
