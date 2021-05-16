e390d34e1ab0c41c9e402c04fe0929a9fb831dde
Phong <phong940253@gmail.com>
2021-05-16 11:52:33 +0700

    add cookie user api

---
+++ js/Login.js
@@ -0,0 +1,41 @@
+// function to set cookie
+function setCookie(cname, cvalue, exdays) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// get or read cookie
+function getCookie(cname){
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function showHomePage(){
+
+    // validate jwt to verify access
+    let jwt = getCookie('jwt');
+    $.post("api/validate_token.php", JSON.stringify({ jwt:jwt })).done(function(result) {
+
+        // home page html will be here
+    })
+
+    // show login page on error will be here
+}
+
+// getCookie() will be here
+
+// showLoggedInMenu() will be here
