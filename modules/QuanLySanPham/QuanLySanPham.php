02d368ca20d613481e7abfa4fec3845c8d16000c
ttphuongvi <trthphuongvi2901@gmail.com>
2021-05-16 15:21:57 +0700

    Sửa lỗi css, thêm các mục tùy chọn

--- modules/QuanLySanPham/QuanLySanPham.php
+++ modules/QuanLySanPham/QuanLySanPham.php
@@ -6,8 +6,8 @@
   </head>
   <body>
     <?php
-    include_once '../../vendor/autoload.php';
-    require("../../inc/header.php");
+    include_once $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
+    include_once $_SERVER['DOCUMENT_ROOT']."/inc/header.php";
     ?>
     <div class="PhanChung">
       <div class="KhungChung">
@@ -30,7 +30,9 @@
                   height="1em"
                   width="1em"
                   xmlns="http://www.w3.org/2000/svg"
-                ></svg>
+                >
+                    <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
+                </svg>
                 <span>Thông tin sản phẩm</span>
               </a>
             </li>
@@ -48,11 +50,81 @@
                 <span>Thông báo của tôi</span>
               </a>
             </li>
-            <li><a class="Quan"</li>
+            <li>
+              <a class="Quan-ly-don-hang">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Quản lý đơn hàng</span>
+              </a>
+            </li>
+            <li>
+              <a class="So-dia-chi">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Sổ địa chỉ</span>
+              </a>
+            </li>
+            <li>
+              <a class="Thong-tin-thanh-toan">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Thông tin thanh toán</span>
+              </a>
+            </li>
+            <li>
+              <a class="Nhan-xet">
+                  <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                       viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                  </svg>
+                <span>Nhận xét sản phẩm đã mua</span>
+              </a>
+            </li>
+            <li>
+              <a class="San-pham-da-xem">
+                  <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Sản phẩm đã xem</span>
+              </a>
+            </li>
+            <li>
+              <a class="San-pham-yeu-thich">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Sản phẩm yêu thích</span>
+              </a>
+            </li>
+            <li>
+              <a class="Mua-sau">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Mua sau</span>
+              </a>
+            </li>
+            <li>
+              <a class="Hoi-dap">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Hỏi đáp</span>
+              </a>
+            </li>
+            <li>
+              <a class="Ma-giam-gia">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                     viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <span>Mã giảm giá</span>
+              </a>
+            </li>
           </ul>
         </aside>
         <div class="ThongTinSanPham">
-          <h3 class="HeaderThongtinsanpham"></h3>
           <div class="KhungThongTinSanPham">
             <form>
               <div class="Khung-form">
@@ -125,7 +197,7 @@
       </div>
     </div>
     <?php
-    include_once '../../inc/footer.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/inc/footer.php';
     ?>
   </body>
 </html>
