1964f4cf88a1a798e26c6afee03b8a49bdcd2055
Phong <phong940253@gmail.com>
2021-05-19 15:34:04 +0700

    add edit product api

--- api/product.php
+++ api/product.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/api/restfull.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/api/restful.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/DatabaseConnector.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php";
 
