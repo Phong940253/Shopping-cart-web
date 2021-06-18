c85e0ef3ce8c2d016ba69c33ee749636f9565d70
Phong <phong940253@gmail.com>
2021-06-18 09:55:31 +0700

    fix cache service worker

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,11 +1,9 @@
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/sw.js").then(function(registration) {
-      // Registration was successful
-      console.log("ServiceWorker registration successful with scope: ", registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log("ServiceWorker registration failed: ", err);
-    });
-  });
+  navigator.serviceWorker.register("/service-worker.js")
+      .then(function(registration) {
+        console.log("Registration successful, scope is:", registration.scope);
+      })
+      .catch(function(error) {
+        console.log("Service worker registration failed, error:", error);
+      });
 }
