9d85aeb4866c28f83f82dfe87b79c2267850a590
Phong <phong940253@gmail.com>
2021-06-18 10:35:44 +0700

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,17 @@
-const CACHE_NAME = "my-site-cache-v1";
+const CACHE_NAME = "my-site-cache-v2";
 const urlsToCache = [
   "/lib/jquery.min.js",
   "/lib/slick/slick.js",
   "/img/icon.png",
+  "/img/google-play-badge.png",
+  "/img/app-store.svg",
+  "/img/youtube.svg",
+  "/img/fb.svg",
+  "/img/arow.png",
 ];
 
 self.addEventListener("install", function(event) {
+  console.log("Service worker installing...");
   // Perform install steps
   event.waitUntil(
       caches.open(CACHE_NAME)
@@ -17,6 +23,7 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
+  console.log("Service worker activating...");
   event
       .respondWith(caches.match(event.request)
           .then(function(response) {
@@ -29,17 +36,19 @@ self.addEventListener("fetch", function(event) {
       );
 });
 
-// self.addEventListener("activate", function(event) {
-//   const cacheWhitelist = ["pages-cache-v1", "blog-posts-cache-v1"];
-//   event.waitUntil(
-//       caches.keys().then(function(cacheNames) {
-//         return Promise.all(
-//             cacheNames.map(function(cacheName) {
-//               if (cacheWhitelist.indexOf(cacheName) === -1) {
-//                 return caches.delete(cacheName);
-//               }
-//             }),
-//         );
-//       }),
-//   );
-// });
+
+self.addEventListener("activate", function(event) {
+  const cacheWhitelist = ["my-site-cache-v2"];
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.map(function(cacheName) {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            }),
+        );
+      }),
+  );
+  console.log("update cache success");
+});
